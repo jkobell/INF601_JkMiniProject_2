da76d58de0e7d021b7fa5adfda147f99913cb39b
jk185256 <jk185256@ncr.com>
2022-09-24 17:37:26 -0500

    list_of_dfs_group_by_year

--- main.py
+++ main.py
@@ -22,20 +22,29 @@ while (remain_count >= 20):
 print(len(df_sets))
 df_years = []
 begin_row = df.head(n=1)
+end_row = df.tail(n=1)
 begin_date = pd.to_datetime(begin_row['Date'])
+end_date = pd.to_datetime(end_row['Date'])
 begin_date_int32 = begin_date.dt.year.astype(int).values[0]
 begin_year = str(begin_date_int32) #int64 to string
-end_row = df.tail(n=1)
-end_date = pd.to_datetime(end_row['Date'])
 end_date_int32 = end_date.dt.year.astype(int).values[0]
 end_year = str(end_date_int32) #int64 to string
 years_range = end_date_int32 - begin_date_int32 + 1
+for i in range(years_range):
+    if begin_year:
+        df_year = df[(df['Date'] > f"{begin_year}-01-01") & (df['Date'] < f"{begin_year}-12-31")]
+        begin_date_int32 += 1
+        begin_year = str(begin_date_int32)
+        df_years.append(df_year)
+
 print(begin_year)
 print(end_year)
 print(years_range)
-df_year = df[(df['Date'] > f"{begin_year}-01-01") & (df['Date'] < f"{begin_year}-12-31")]
+print(len(df_years))
 
-print(df_year)
+for year in df_years:
+    print(year)
+#print(df_year)
 jj = 66
 
 
