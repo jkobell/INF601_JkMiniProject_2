44c60d132deb0d3174ecf82943e045c7224c8c3c
jk185256 <jk185256@ncr.com>
2022-09-24 20:54:59 -0500

    each_year_of_monthly_averages_to_DataFrame

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import logging
 import pandas as pd
-import dateparser as dp
+import matplotlib.pyplot as plt
 logging.basicConfig(filename='app_log.txt', format='%(asctime)s %(message)s' ,encoding='utf-8') # log with timestamp and message. Do not print to console
 data_filename = ('app_data/TSLA.csv')
 try: # log if exception
     df = pd.read_csv(data_filename) # input data from csc into dataframe
 except Exception as Argument:
     logging.exception(f"An error occured while creating a dataframe from {data_filename} | Error: {str(Argument)}")
-df_sets = []
-first_row_index = 0
-last_row_index = 20
-increment = 20
-remain_count = len(df)
-while (remain_count >= 20):
-    df_slice = df.iloc[first_row_index:last_row_index]
-    df_sets.append(df_slice)
-    first_row_index += 20
-    last_row_index += 20
-    remain_count -= 20
-    print(df_slice)
-print(len(df_sets))
+#df_sets = []
+#first_row_index = 0
+#last_row_index = 20
+#increment = 20
+#remain_count = len(df)
+#while (remain_count >= 20):
+    #df_slice = df.iloc[first_row_index:last_row_index]
+    #df_sets.append(df_slice)
+    #first_row_index += 20
+    #last_row_index += 20
+    #remain_count -= 20
+    #print(df_slice)
+#print(len(df_sets))
 df_years = []
 begin_df_row = df.head(n=1)
 end_df_row = df.tail(n=1)
@@ -41,7 +41,7 @@ for i in range(years_df_range):
 #print(end_year)
 #print(years_range)
 #print(len(df_years))
-df_months = []
+#df_months = []
 for item in df_years:
     df_item = pd.DataFrame(item) # cast as a DataFrame
     begin_year_df_row = df_item.head(n=1)
@@ -55,21 +55,34 @@ for item in df_years:
     end_month_df_date_int32 = end_year_df_date.dt.month.astype(int).values[0]
     end_month_df_date = str(end_month_df_date_int32) #int32 to string
     months_df_range = end_month_df_date_int32 - begin_month_df_date_int32 + 1
+    df_months = []
     for i in range(months_df_range):
         if begin_month_df_date:
-            if len(begin_month_df_date) == 1:
+            if len(begin_month_df_date) == 1: # if month digits is less than 2
                 begin_month_df_date = f"0{begin_month_df_date}"
         df_month = df_item[(df_item['Date'] >= f"{begin_year_df_year}-{begin_month_df_date}-01") & (df_item['Date'] <= f"{begin_year_df_year}-{begin_month_df_date}-31")]
         begin_month_df_date_int32 += 1
         begin_month_df_date = str(begin_month_df_date_int32)
         df_months.append(df_month)
+    close_average = []
+    for month_item in df_months:
+        df_month_item = pd.DataFrame(month_item) # cast as a DataFrame
+        month_item_count = len(df_month_item)
+        month_close_prices_sum = df_month_item['Close'].sum()
+        if ((month_close_prices_sum >= 0) & (month_item_count >= 1)): # check for values            
+            month_close_average = month_close_prices_sum / month_item_count
+            close_average.append(month_close_average)
+    close_average_df = pd.DataFrame(close_average)
+    print(close_average_df)
+        
+
 
 
     
     
-    print(months_df_range)
-    print(len(df_month))
-    print(len(df_months))
+    #print(months_df_range)
+    #print(len(df_month))
+    #print(len(df_months))
     #print(len(item))
     
     #print(item)
