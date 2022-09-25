0ae1e30d6ffdd7ff40ae398a0c403fb6887b8c18
jk185256 <jk185256@ncr.com>
2022-09-25 12:50:45 -0500

    draft

--- main.py
+++ main.py
@@ -2,26 +2,13 @@ import logging
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-logging.basicConfig(filename='app_log.txt', format='%(asctime)s %(message)s' ,encoding='utf-8') # log with timestamp and message. Do not print to console
+logging.basicConfig(filename='app_log.txt', format='%(asctime)s %(message)s' ,encoding='utf-8') # log with timestamp and message.
 ticker = 'TSLA' # todo: make interactive with input control
 data_filename = (f"app_data/{ticker}.csv")
 try: # log if exception
-    df = pd.read_csv(data_filename) # input data from csc into dataframe
+    df = pd.read_csv(data_filename) # input data from csv into dataframe
 except Exception as Argument:
     logging.exception(f"An error occured while creating a dataframe from {data_filename} | Error: {str(Argument)}")
-#df_sets = []
-#first_row_index = 0
-#last_row_index = 20
-#increment = 20
-#remain_count = len(df)
-#while (remain_count >= 20):
-    #df_slice = df.iloc[first_row_index:last_row_index]
-    #df_sets.append(df_slice)
-    #first_row_index += 20
-    #last_row_index += 20
-    #remain_count -= 20
-    #print(df_slice)
-#print(len(df_sets))
 df_years = []
 begin_df_row = df.head(n=1)
 end_df_row = df.tail(n=1)
@@ -38,12 +25,6 @@ for i in range(years_df_range):
         begin_year_df_date_int32 += 1
         begin_df_year = str(begin_year_df_date_int32)
         df_years.append(df_year)
-
-###print(begin_year)
-#print(end_year)
-#print(years_range)
-#print(len(df_years))
-#df_months = []
 for item in df_years:
     df_item = pd.DataFrame(item) # cast as a DataFrame
     begin_year_df_row = df_item.head(n=1)
@@ -74,8 +55,6 @@ for item in df_years:
         month_end_df_row = df_month_item.tail(n=1)
         month_begin_df_date = pd.to_datetime(month_begin_df_row['Date'])
         month_end_df_date = pd.to_datetime(month_end_df_row['Date'])
-        #month_begin_df_date_str = month_begin_df_date.dt.month.astype(str).values[0]
-        #month_end_df_date_str = month_end_df_date.dt.month.astype(str).values[0]
         month_begin_df_date_str = month_begin_df_date.astype(str).values[0]
         month_end_df_date_str = month_end_df_date.astype(str).values[0]
         month_xtick_label = f"{month_begin_df_date_str}\n- {month_end_df_date_str}"
@@ -93,23 +72,11 @@ for item in df_years:
     plt.title(chart_id)
     plt.xticks(np.arange(len(close_average)), months_xticks_labels, rotation = 70)
     plt.tight_layout()
-    plt.savefig(f"charts/{chart_id}.png", facecolor = "#cfd9e4") # todo: wrap in try except, log except
-    plt.show()
-    #print(close_average_df)
-        
-
-
-
-    
-    
-    #print(months_df_range)
-    #print(len(df_month))
-    #print(len(df_months))
-    #print(len(item))
-    
-    #print(item)
-    #print(df_year)
-jj = 66
+    try: # log if exception
+        plt.savefig(f"charts/{chart_id}.png", facecolor = "#cfd9e4") # throw if exception during savefig
+    except Exception as Argument:
+        logging.exception(f"An error occured while saving chart: {chart_id} | Error: {str(Argument)}")    
+    #plt.show()
 
 
 
